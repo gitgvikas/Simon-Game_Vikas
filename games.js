0b0ce56c4fc82307c1cf3fb371e0bbf454ab281f
gitgvikas <83117628+gitgvikas@users.noreply.github.com>
2023-05-19 18:41:16 +0530

    Update games.js

--- games.js
+++ games.js
@@ -17,7 +17,7 @@ function nextSequence(){
 }
 
 function playSound(name){
-    var audio = new Audio("https://gitgvikas.gitub.io/Simon-Game_Vikas/"+ name + ".mp3");
+    var audio = new Audio("https://github.com/gitgvikas/Simon-Game_Vikas/raw/main/"+ name + ".mp3?raw=true");
     audio.play();
 }
 
